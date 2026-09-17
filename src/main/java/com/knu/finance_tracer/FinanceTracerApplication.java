1e97e8b162d6397ae0a262a3aef28d60aa944938
vbanasevych <viktoriia_banasevych@epam.com>
2026-09-17 14:03:13 +0300

    Initial commit

---
+++ src/main/java/com/knu/finance_tracer/FinanceTracerApplication.java
@@ -0,0 +1,13 @@
+package com.knu.finance_tracer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceTracerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceTracerApplication.class, args);
+	}
+
+}
